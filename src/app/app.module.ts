7d21c46038d5bcdb8ac438e3a96e8deeb94acc1f
dave0022 <deiviss50@gmail.com>
2017-10-31 14:15:33 +0100

    Connected to the CustomerBO

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,45 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { CustomerComponent } from './customers/customer/customer.component';
+import { CustomerService } from './customers/shared/customer.service';
+import {HttpClientModule} from '@angular/common/http';
+import { CustomComponent } from './customers/custom/custom.component';
+import { CustomerListComponent } from './customers/customer-list/customer-list.component';
+import { CustomerDetailComponent } from './customers/customer-detail/customer-detail.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const appRoutes: Routes = [
+  { path: 'customer/:id',
+    component: CustomerDetailComponent },
+  {
+    path: 'customers',
+    component: CustomerListComponent,
+    data: { title: 'Customer List' }
+  },
+  { path: '',
+    redirectTo: '/customers',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CustomerComponent,
+    CustomComponent,
+    CustomerListComponent,
+    CustomerDetailComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    RouterModule.forRoot(appRoutes)
   ],
-  providers: [],
+  providers: [CustomerService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
